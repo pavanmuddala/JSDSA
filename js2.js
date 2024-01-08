ca083c3408a8629f07629c7f3a00bf5f8711ebcb
pavanmmgs <pavanmuddala@gmail.com>
2024-01-08 23:11:46 +0530

    factorial function

---
+++ js2.js
@@ -0,0 +1,12 @@
+console.log("day 2 of js dsa")
+
+const factorial=(n)=>{
+    let result=1
+    for(let i=2;i<=n;i++){
+        result=result*i
+    }
+    return result
+}
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(5))
