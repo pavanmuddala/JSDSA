dd9b305c18bdf3a3ab441f1220b717599dcf58da
pavanm <pavanmuddala7654@gmail.com>
2024-04-30 00:29:13 +0530

    hash table completed

--- HashTable.js
+++ HashTable.js
@@ -19,3 +19,42 @@
 // 1. Hash tables are typically implemented where contant time lookup and insertion are required
     // Example database indexing and caching 
 
+class HashTable{
+    constructor(size){
+        this.size=size;
+        this.table=new Array(size);
+    }
+    hash(key){
+        let total=0
+        for(let i=0;i<key.length;i++){
+            total+=key.charCodeAt(i);
+        }
+        return total%this.size
+    }
+    set(key,value){
+        
+        let index=this.hash(key);
+        console.log("index ...",index)
+        this.table[index]=value;
+    }
+    get(key){
+        let index=this.hash(key);
+        return this.table[index];
+    }
+    remove(key){
+        let index=this.hash(key);
+        this.table[index]=undefined;
+    }
+}
+
+const hashTable=new HashTable(50);
+hashTable.set("name","pavan");
+hashTable.set("address","medapadu");
+hashTable.set("phno",7993011526);
+console.log(hashTable.get("name"));
+console.log(hashTable.get("address"));
+console.log(hashTable.get("phno"));
+hashTable.remove("name");
+console.log(hashTable.get("name"));
+
+
